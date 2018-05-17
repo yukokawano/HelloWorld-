9886328c98b881ddd13f24c85ec08fd90f9c7df1
yuko.kawano <mumiopop@mac.com>
2018-05-17 19:23:48 +0900

    Hello Worldのラベルを追加した。

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by mumio on 2018/04/07.
+//  Copyright © 2018年 yuuko,kawano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
